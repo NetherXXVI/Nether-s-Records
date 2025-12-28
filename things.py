7bfdcf68ecb72e487037109a224ac0dccde2320e
NetherXXVI <akktheodor@gmail.com>
2025-12-28 14:00:26 +0200

    Create things.py

---
+++ things.py
@@ -0,0 +1,3 @@
+gh label create "subscription: standard" --color 0366d6 --description "Standard features for individual creators" --repo NetherXXVI/Nether-s-Records
+gh label create "subscription: premium" --color ffbf00 --description "All features + priority support" --repo NetherXXVI/Nether-s-Records
+gh label create "subscription: enterprise" --color d73a49 --description "Team/label plan with advanced features & billing" --repo NetherXXVI/Nether-s-Records
